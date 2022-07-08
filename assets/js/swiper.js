c2deb3d6d8558beca5ae6dfec7343fbb54341942
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-07-08 13:01:29 +0300

    add: block open

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,21 +2,39 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   slidesPerView: 5,
   slidesPerGroup: 1,
+  // centeredSlides: true,
   // draggable: true,
   // allowTouchMove: true,
-
+  // slideToClickedSlide: true,
+  // loop: true,
+  // loopedSlides: 5,
+  // effect: "coverflow",
+  // coverflow: {
+  //   rotate: 40,
+  //   stretch: 0,
+  //   depth: 50,
+  //   modifier: 1,
+  //   slideShadows: false,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
+    2000: {
+      slidesPerView: 5,
+      slidesPerGroup: 1,
+      spaceBetween: 23,
+    },
     1220: {
       slidesPerView: 4,
       slidesPerGroup: 1,
+      spaceBetween: 23,
     },
     1120: {
       slidesPerView: 3,
       slidesPerGroup: 1,
+      spaceBetween: 23,
     },
   },
 });
