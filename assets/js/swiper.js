53ffa1b35016d5b71423544f5259029e4fa81b8f
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-07-11 22:12:15 +0300

    fix: slider

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,20 +2,7 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   slidesPerView: 4,
   slidesPerGroup: 1,
-  // centeredSlides: true,
-  // draggable: true,
-  // allowTouchMove: true,
-  // slideToClickedSlide: true,
-  // loop: true,
-  // loopedSlides: 5,
-  // effect: "coverflow",
-  // coverflow: {
-  //   rotate: 40,
-  //   stretch: 0,
-  //   depth: 50,
-  //   modifier: 1,
-  //   slideShadows: false,
-  // },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
