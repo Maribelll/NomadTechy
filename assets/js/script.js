62bcacb743c7bc1d667b391a32ba09cb9b4e345d
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-07-12 21:12:32 +0300

    add: new email php

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,47 @@
 jQuery(function ($) {
-  $("#my_form_email input, #my_form_email textarea").bind(
-    "keyup",
-    function (_event) {
-      var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\[a-z]{2,6}$/i;
-      var mail = $("#my_form_email .email");
+  $("#my_form_email input, #my_form_email textarea").bind("keyup", function () {
+    var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\[a-z]{2,6}$/i;
+    var mail = $("#my_form_email .email");
 
-      // if(mail.val().search(pattern) == 0){
-      //      	$('#submit_modal_err').css('display', "none");
-      //    		$('#submit_modal').css('display', "block");
-      // }else{
-      //      	$('#submit_modal_err').css('display', "block");
-      //    		$('#submit_modal').css('display', "none");
-      // }
+    // if(mail.val().search(pattern) == 0){
+    //      	$('#submit_modal_err').css('display', "none");
+    //    		$('#submit_modal').css('display', "block");
+    // }else{
+    //      	$('#submit_modal_err').css('display', "block");
+    //    		$('#submit_modal').css('display', "none");
+    // }
 
-      if (
-        $("#my_form_email .name").val() != "" &&
-        $("#my_form_email .company").val() != "" &&
-        $("#my_form_email .email").val() != "" &&
-        $("#my_form_email .phoneNumber").val() != "" &&
-        $("#my_form_email .message").val() != "" &&
-        mail.val().search(pattern) == 0
-      ) {
-        $("#submit_modal_err").css("display", "none");
-        $("#form_err").css("display", "none");
-        $("#submit_modal").css("display", "block");
-      }
-
-      if ($("#my_form_email .name").val() != "") {
-        $("#my_form_email .name").css("border", "none");
-      }
-      if ($("#my_form_email .company").val() != "") {
-        $("#my_form_email .company").css("border", "none");
-      }
-      if ($("#my_form_email .email").val() != "") {
-        $("#my_form_email .email").css("border", "none");
-      }
-      if ($("#my_form_email .phoneNumber").val() != "") {
-        $("#my_form_email .phoneNumber").css("border", "none");
-      }
-      if ($("#my_form_email .message").val() != "") {
-        $("#my_form_email .message").css("border", "none");
-      }
+    if (
+      $("#my_form_email .name").val() != "" &&
+      $("#my_form_email .company").val() != "" &&
+      $("#my_form_email .email").val() != "" &&
+      $("#my_form_email .phoneNumber").val() != "" &&
+      $("#my_form_email .message").val() != "" &&
+      mail.val().search(pattern) == 0
+    ) {
+      $("#submit_modal_err").css("display", "none");
+      $("#form_err").css("display", "none");
+      $("#submit_modal").css("display", "block");
     }
-  );
 
-  $("#submit_modal_err").bind("click", function (_event) {
+    if ($("#my_form_email .name").val() != "") {
+      $("#my_form_email .name").css("border", "none");
+    }
+    if ($("#my_form_email .company").val() != "") {
+      $("#my_form_email .company").css("border", "none");
+    }
+    if ($("#my_form_email .email").val() != "") {
+      $("#my_form_email .email").css("border", "none");
+    }
+    if ($("#my_form_email .phoneNumber").val() != "") {
+      $("#my_form_email .phoneNumber").css("border", "none");
+    }
+    if ($("#my_form_email .message").val() != "") {
+      $("#my_form_email .message").css("border", "none");
+    }
+  });
+
+  $("#submit_modal_err").bind("click", function () {
     $("#form_err").css("display", "block");
     if ($("#my_form_email .name").val() == "") {
       $("#my_form_email .name").css("border", "solid thin #f00");
@@ -70,7 +67,7 @@ jQuery(function ($) {
     }
   });
 
-  $("#submit_modal").bind("click", function (_event) {
+  $("#submit_modal").bind("click", function () {
     $(".thankyou").css("display", "block");
   });
 });
